18abc9d07ce9e004564b09764e01c2564713465c
bahing-rai <mahchaibahing@gmail.com>
2025-04-21 23:19:11 +0545

    added

---
+++ app/src/main/java/com/example/task1/lesson4.kt
@@ -0,0 +1,35 @@
+package com.example.task1
+
+fun main() {
+    var age = arrayOf(1, 2, 3)
+    println(age)
+    println("The first element of age is " + age[0])
+    println("The second element of age is " + age[1])
+    println("The third element of age is" + age[2])
+
+    println("*")
+
+    var name = arrayOf("ram", "shyam", "Hari")
+    name[1] = "sandis"
+    println("The first element of name is " + name[0])
+    println("The second element of name is " + name[1])
+    println("The third element of name is " + name[2])
+
+    var age1 = ArrayList<Int>()
+    age1.add(1)
+    age1.add(1, 20)
+    age1.add(5)
+//we can add value directly in array while initializing variables.
+    var age2 = arrayListOf<Int>(1, 20, 5)
+    var name = arrayListOf<String>(
+        "sandis", "ram", "shyam"
+    )
+    name.add("hari")
+    name.add(4, "Sita")
+    name.remove("shyam")
+    name.removeAt(0)
+    var mixArrayList = arrayListOf<Any>("hello", 5, 2.0)
+    println(mixArrayList[0]);
+    println(mixArrayList[1]);
+    println(mixArrayList[2]);
+}
